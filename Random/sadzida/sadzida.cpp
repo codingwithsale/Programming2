6f398bd887c210813aa8926ccd2142d243c681cb
codingwithsale <103439745+codingwithsale@users.noreply.github.com>
2023-05-08 18:10:39 +0200

    Task for a friend

---
+++ Random/sadzida/sadzida.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<ctime>
+using namespace std;
+
+string uniqueCharacters(string str){
+    string new_str;
+    new_str += str[0];
+    for (int i = 1; i < str.length(); i++){
+        int counter = 0;
+        for (int j = 0; j < new_str.length(); j++){
+            if (str[i] == new_str[j]){
+                counter++;
+            }
+        }
+        if (counter == 0){
+            new_str += str[i];
+        }
+    }
+    return new_str;
+};
+
+int main(){
+    
+    string str1 = "ababccddde";
+
+    cout << uniqueCharacters(str1);
+    
+    return 0;
+}
